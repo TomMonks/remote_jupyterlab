2f7d2a2b9690bdc557d3c2bd2844c3ed473d2a14
TomMonks <thomas.m.monks@gmail.com>
2026-09-23 12:09:07 +0100

    feat(openstack) + pem key and conda version of script

---
+++ openstack-pem/launch_jupyter.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Default values
+CONFIG_FILE=""
+REMOTE_HOST=""
+REMOTE_USER="ubuntu"
+PEM_KEY=""
+ENV_NAME="hds_python"
+PORT=4569
+REMOTE_DIR="hpdm171"
+
+# Parse command-line options
+while getopts "f:e:p:d:h:i:u:" opt; do
+  case $opt in
+    f) CONFIG_FILE="$OPTARG" ;;
+    e) ENV_NAME="$OPTARG" ;;
+    p) PORT="$OPTARG" ;;
+    d) REMOTE_DIR="$OPTARG" ;;
+    h) REMOTE_HOST="$OPTARG" ;;
+    i) PEM_KEY="$OPTARG" ;;
+    u) REMOTE_USER="$OPTARG" ;;
+    *) echo "Usage: $0 [-f config_file] [-h remote_ip] [-i pem_key] [-u remote_user] [-e env] [-p port] [-d dir]"; exit 1 ;;
+  esac
+done
+
+# Read config file if specified
+if [ -n "$CONFIG_FILE" ]; then
+  while IFS='=' read -r key value; do
+    case $key in
+      "REMOTE_HOST") REMOTE_HOST="$value" ;;
+      "REMOTE_USER") REMOTE_USER="$value" ;;
+      "PEM_KEY") PEM_KEY="$value" ;;
+      "ENV_NAME") ENV_NAME="$value" ;;
+      "PORT") PORT="$value" ;;
+      "REMOTE_DIR") REMOTE_DIR="$value" ;;
+    esac
+  done < "$CONFIG_FILE"
+fi
+
+# SSH tunnel in background
+ssh -i "$PEM_KEY" -N -L localhost:${PORT}:localhost:${PORT} ${REMOTE_USER}@${REMOTE_HOST} &
+TUNNEL_PID=$!
+
+# Build remote command
+REMOTE_CMD="source ~/.miniconda3/bin/activate ${ENV_NAME} && "
+if [ -n "$REMOTE_DIR" ]; then
+  REMOTE_CMD+="cd ${REMOTE_DIR} && "
+fi
+REMOTE_CMD+="jupyter lab --no-browser --port=${PORT}"
+
+# Start JupyterLab
+ssh -i "$PEM_KEY" ${REMOTE_USER}@${REMOTE_HOST} "$REMOTE_CMD"
+
+# Cleanup
+kill $TUNNEL_PID
